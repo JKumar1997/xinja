6b3f43d676cc2df7bc00fdb38fc413c4dc321997
JKumar1997 <jason.kumar@student.uts.edu.au>
2020-04-07 21:30:45 +1000

    fixes
    removed old p5js
    updated svg
    added light gradient to svg
    reformat

--- sketch.js
+++ sketch.js
@@ -2,24 +2,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-	//xinja.display();
-} 
-
-
-/*
-function set_gradient(colour_a, colour_b) {
-  for (var i = 0; i < height; i++) {
-    var x = map(i, 0, height, 0, 1);
-    var colour = lerpColor(colour_a, colour_b, x);
-    stroke(colour);
-    var y = line(0, i, width, i);
-    fill(y);
-  }
-}
-*/
-
-
 function windowResized() {
 	  resizeCanvas(windowWidth, windowHeight);
 }
