779ee7b1d95cf027c2b06ac476467d16b1056c0e
JKumar1997 <jason.kumar@student.uts.edu.au>
2020-04-07 21:21:49 +1000

    eyepiece

--- sketch.js
+++ sketch.js
@@ -1,23 +1,11 @@
-let xinja;
-
 function setup() { 
   createCanvas(windowWidth, windowHeight);
-  xinja = new Xinja();
 }
 
 function draw() {
-	xinja.display();
+	//xinja.display();
 } 
 
-class Xinja {
-	display() {
-		noStroke();
-		fill(255);
-		ellipse(windowWidth/2-57, windowHeight/2-22, 54, 54);
-		ellipse(windowWidth/2+57, windowHeight/2-22, 54, 54);
-		rect(windowWidth/2-55, windowHeight/2-49, 110 ,54);
-	}
-}
 
 /*
 function set_gradient(colour_a, colour_b) {
