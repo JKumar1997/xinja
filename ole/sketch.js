3edd6838c3504c6ad9dca873d8cd2f8f4cafaeef
JKumar1997 <jason.kumar@student.uts.edu.au>
2020-04-07 20:21:36 +1000

    redone w/ svg + css

--- ole/sketch.js
+++ ole/sketch.js
@@ -2,7 +2,14 @@ let xinja;
 
 function setup() { 
   createCanvas(windowWidth, windowHeight);
+  bg_colour1 = color(226, 25, 112);
+  bg_colour2 = color(118, 67, 148);
+  bg_colour3 = color(115, 121, 136);
   xinja = new Xinja();
+  drop_shadow = color(0);
+  head_colour = color(random(1, 256), random(1, 256), random(1,256));
+  white = color(255);
+
 }
 
 function draw() {
@@ -10,31 +17,39 @@ function draw() {
 } 
 
 class Xinja {
+	constructor() {
+	}
 	display() {
+		//Drop Shadow
+		drop_shadow.setAlpha(64);
+		fill(drop_shadow);
 		noStroke();
+		ellipse(windowWidth/2+10, windowHeight/2+2, 245, 245);	
+		//Head
+		//fill(set_gradient(white, head_colour));
+		noStroke();
+		ellipse(windowWidth/2, windowHeight/2, 245, 245);
+		//Cutey Eyes
 		fill(255);
 		ellipse(windowWidth/2-57, windowHeight/2-22, 54, 54);
 		ellipse(windowWidth/2+57, windowHeight/2-22, 54, 54);
 		rect(windowWidth/2-55, windowHeight/2-49, 110 ,54);
+		noFill();
 	}
 }
 
-/*
+
 function set_gradient(colour_a, colour_b) {
   for (var i = 0; i < height; i++) {
     var x = map(i, 0, height, 0, 1);
     var colour = lerpColor(colour_a, colour_b, x);
     stroke(colour);
-    var y = line(0, i, width, i);
-    fill(y);
+    line(0, i, width, i);
   }
 }
-*/
-
 
 function windowResized() {
 	  resizeCanvas(windowWidth, windowHeight);
 }
-
 
 
