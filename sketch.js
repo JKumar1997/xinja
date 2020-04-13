2f6325ba1ce3591719a72ebc1b02125d7ffb9ff5
JKumar1997 <jason.kumar@student.uts.edu.au>
2020-04-13 14:09:19 +1000

    added eyes, eyecolour, organised files

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function updateHead(color) {
+	const gradientEndEl = document.querySelector('#gradientEnd');
+	gradientEndEl.setAttribute('stop-color', `#${color}`);
+}
+
+function updateEyepiece(color) {
+	const gradientEyepiece = document.querySelector('#gradientEndEye');
+	gradientEyepiece.setAttribute('stop-color', `#${color}`);
+}
+     
+function updateShadoweye(color) {
+	const gradientShadow = document.querySelector('#gradientShadoweye');
+	gradientShadow.setAttribute('stop-color', `#${color}`);
+}
+      
+function updateShadow(color) {
+	const updateShadowMain = document.querySelector('#mainshadow');
+	updateShadowMain.setAttribute('stop-color', `#${color}`);
+}
+
+function updateEyes(color) {
+   const updateEye = document.querySelector('#eye');
+   updateEye.setAttribute('stop-color', `#${color}`);
+}
