0dffd67c193156afb4acf58e97ad09fd982c57e9
JKumar1997 <jason.kumar@student.uts.edu.au>
2020-04-14 18:03:57 +1000

    mouth + color change/picker

--- sketch.js
+++ sketch.js
@@ -22,3 +22,13 @@ function updateEyes(color) {
    const updateEye = document.querySelector('#eye');
    updateEye.setAttribute('stop-color', `#${color}`);
 }
+
+function updateMouth(color) {
+   const mouth = document.querySelector('#mouthgradienttop');
+   mouth.setAttribute('stop-color', `#${color}`);
+}
+
+function updateMouthshadow(color) {
+   const mouthshadow = document.querySelector('#mouthgradientbottom');
+   mouthshadow.setAttribute('stop-color', `#${color}`);
+}
